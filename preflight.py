fc26fcfc68f09dca9a23f45d07fdececa8cd5300
Aaron Farnham <aaron@foreflight.com>
2013-09-18 10:47:25 -0500

    Added arm64 architecture

--- preflight.py
+++ preflight.py
@@ -16,7 +16,7 @@ def min_deployment_version():
     return '7.0'
 
 def architectures(platform):
-    return {IPHONE_PLATFORM: ['armv7', 'armv7s'],
+    return {IPHONE_PLATFORM: ['armv7', 'armv7s', 'arm64'],
             SIM_PLATFORM: ['i386']}[platform]
 
 def get_platforms():
