6217f750565d6e89d717da208b1af0616f4fd79e
Aaron Farnham <aaron@foreflight.com>
2013-10-04 12:16:25 -0500

    Successfully building spatialite

--- flightplans/libspatialite.py
+++ flightplans/libspatialite.py
@@ -1,6 +1,7 @@
 import os
 import tempfile
 import shutil
+import subprocess
 
 from flightplan import FlightPlan
 
@@ -18,6 +19,7 @@ class SpatialiteFlightPlan(FlightPlan):
         return [
             '--disable-freexl',
             '--disable-geos',
+            '--disable-examples', # needed if --disable-geos is used
         ]
 
     def get_resources(self):
@@ -25,6 +27,15 @@ class SpatialiteFlightPlan(FlightPlan):
             'http://www.gaia-gis.it/gaia-sins/libspatialite-4.1.1.tar.gz'
         ]
         self.download_and_unarchive(urls)
-    
+        self.patch()
+
+    def patch(self):
+        patch_file = 'libspatialite_' + self.get_version() + '.patch'
+        patch_path = os.path.dirname(os.path.abspath(__file__))
+        patch_path = os.path.join(patch_path, patch_file)
+        with open(patch_path) as f:
+            subprocess.call(['patch', '-p1'], stdin=f)
+
+
 FLIGHTPLAN_CLASS = SpatialiteFlightPlan
 
