d201352e7ea96b7f9f503625fd6399bb1f1cd076
Aaron Farnham <aaron@foreflight.com>
2016-07-26 23:19:18 -0500

    updated gdal and proj4 flightplans

--- flightplans/gdal.py
+++ flightplans/gdal.py
@@ -6,28 +6,73 @@ from flightplan import FlightPlan
 
 class GDALFlightPlan(FlightPlan):
     def get_version(self):
-        return '1.10.1'
+        return '2.1.1'
 
     def get_name(self):
         return 'gdal'
-   
+
     def deps(self):
-        return ['proj4', 'sqlite3']
+        return [] # ['proj4', 'sqlite3']
 
     def package_options(self):
         return [
             '--with-static-proj4={prefix}'.format(prefix=self.prefix),
             '--with-unix-stdio-64=no',
             '--with-sqlite3={prefix}'.format(prefix=self.prefix),
-            '--without-gif' #unable to build on x86_64
+            #'--without-gif', #unable to build on x86_64
+            '--with-threads',
+            '--with-ogr',
+            #'--with-geos',
+            '--without-libtool',
+            '--with-libz=internal',
+            '--with-libtiff=internal',
+            '--with-geotiff=internal',
+            '--without-gif',
+            '--without-pg',
+            '--without-grass',
+            '--without-libgrass',
+            '--without-cfitsio',
+            '--without-pcraster',
+            '--without-netcdf',
+            '--without-png',
+            '--without-jpeg',
+            '--without-gif',
+            '--without-ogdi',
+            '--without-fme',
+            '--without-hdf4',
+            '--without-hdf5',
+            '--without-jasper',
+            '--without-ecw',
+            '--without-kakadu',
+            '--without-mrsid',
+            '--without-jp2mrsid',
+            '--without-bsb',
+            '--without-grib',
+            '--without-mysql',
+            '--without-ingres',
+            '--without-xerces',
+            '--without-expat',
+            '--without-odbc',
+            '--without-curl',
+            '--without-sqlite3',
+            '--without-dwgdirect',
+            '--without-panorama',
+            '--without-idb',
+            '--without-sde',
+            '--without-perl',
+            '--without-php',
+            '--without-ruby',
+            '--without-python',
+            '--without-ogpython',
+            '--with-hide-internal-symbols'
         ]
 
     def get_resources(self):
         urls = [
-            'http://download.osgeo.org/gdal/1.10.1/gdal-1.10.1.tar.gz'
+        'http://download.osgeo.org/gdal/2.1.1/gdal-2.1.1.tar.gz'
         ]
         self.download_and_unarchive(urls)
-        self.patch()
+        #self.patch()
 
     def patch(self):
         src_dir = os.path.dirname(os.path.abspath(__file__))
@@ -39,6 +84,8 @@ class GDALFlightPlan(FlightPlan):
         srcfile = os.path.join(src_dir, 'gdal_updated_files', 'config.sub')
         dstfile = os.path.join(dst_dir, 'config.sub')
         shutil.copyfile(srcfile, dstfile)
-    
-FLIGHTPLAN_CLASS = GDALFlightPlan
 
+    def cflags(self):
+        return '-Wno-error=implicit-function-declaration' #fixes builds for 64-bit devices w/ clang
+
+FLIGHTPLAN_CLASS = GDALFlightPlan
